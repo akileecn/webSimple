e2f5da402c8373bf770d001c436f8c0987ceab3b
Administrator <Administrator@192.168.1.6>
2016-06-22 21:10:18 +0800

    微信端页面

--- src/main/webapp/WEB-INF/html/wechat/jquerymobile/js/common.js
+++ src/main/webapp/WEB-INF/html/wechat/jquerymobile/js/common.js
@@ -1,2 +1,27 @@
 //应用路径
 var baseUrl="/web/" ;
+
+
+//清空验证提示信息
+$.fn.clearError = function(error){
+	$(this).find(".col_cv_alt").empty();
+}
+
+//显示验证提示信息
+
+$.fn.showError = function(error){
+	$(this).clearError();
+	if(error){
+		for(var key in error){
+			$(this).find(".col_cv_alt[data-error='"+ key +"']").text(error[key]);			
+		}
+	}
+}
+
+
+function goHome($t){
+	location.href="main.html";
+}
+
+
+
