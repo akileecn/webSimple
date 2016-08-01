d49a3a662f0064ad4598acdcc085c29ed406cbb5
Administrator <Administrator@192.168.1.5>
2016-08-01 23:32:01 +0800

    岗位列表加载

--- src/main/wechat/cn/aki/controller/WechatJobController.java
+++ src/main/wechat/cn/aki/controller/WechatJobController.java
@@ -25,7 +25,7 @@ import cn.aki.service.WechatJobService;
 public class WechatJobController {
 
 	@Autowired
-	WechatJobService wechatJobService ;
+	JobService jobService ;
 	
 	
 	/***
@@ -37,13 +37,13 @@ public class WechatJobController {
 	@ResponseBody
 	@RequestMapping(path="/list",method=RequestMethod.POST)
 	public PageResponse<Job> getJobList(JobQueryForm form){
-		System.out.println("++++++++++++++++++++++++"+form.getPageNum());
-		System.out.println("++++++++++++++++++++++++"+form.getrecruitType());
+		//System.out.println("++++++++++++++++++++++++"+form.getPageNum());
+		//System.out.println("++++++++++++++++++++++++"+form.getrecruitType());
 		PageResponse<Job> response = new PageResponse<Job>();
-		PageInfo<Job> joblist = wechatJobService.getPage(form);
+		PageInfo<Job> joblist = jobService.getPage(form);
 		response.setData(joblist);
-		System.out.println(response);
-		System.out.println("========================");
+		//System.out.println(response);
+		//System.out.println("========================");
 		return response ;
 	}
 	
@@ -57,9 +57,9 @@ public class WechatJobController {
 	@ResponseBody
 	@RequestMapping(path="/detail",method=RequestMethod.POST)
 	public DataResponse<Job> getJobDetail(Integer id){
-		System.out.println("==============="+id);   
+		//System.out.println("==============="+id);   
 		DataResponse<Job> response = new DataResponse<Job>();
-		Job job= wechatJobService.get(id);
+		Job job= jobService.get(id);
 		response.setData(job);
 		return response;
 	}
