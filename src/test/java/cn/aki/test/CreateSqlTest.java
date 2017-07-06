2bb8a2a614014a5f4b1227e86c058f3cf2332144
Akilee <akilee@qq.com>
2017-07-06 15:11:45 +0800

    删除遗留test

--- src/test/java/cn/aki/test/CreateSqlTest.java
+++
@@ -1,9 +0,0 @@
-package cn.aki.test;
-
-public class CreateSqlTest {
-
-	public static void main(String[] args) throws Exception{
-//		CreateSql.newInstance(StaticPage.class).create();
-		CreateSql.simpelCreate("dict.ftl", null);
-	}
-}
--- src/test/java/cn/aki/test/template/dao.ftl
+++
@@ -1,7 +0,0 @@
-package cn.aki.dao;
-
-import cn.aki.entity.${type};
-
-public interface ${type}Mapper extends BaseResumeSubMapper<${type}>{
-
-}
