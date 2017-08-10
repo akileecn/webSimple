7e2a6fa63858bae8119e5fd99311f6c5565b249c
Akilee <akilee@qq.com>
2017-08-10 17:12:31 +0800

    自动生成api文档

--- src/main/java/cn/aki/Application.java
+++ src/main/java/cn/aki/Application.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by Administrator on 2017/7/4.
@@ -12,6 +13,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @MapperScan("cn.aki.dao") // mybatis
 @EnableConfigurationProperties(MyProperties.class) // 自定配置文件
+@EnableSwagger2 // 启动Swagger
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/main/java/cn/aki/response/Response.java
+++ src/main/java/cn/aki/response/Response.java
@@ -1,47 +1,33 @@
 package cn.aki.response;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
 /**
  * 响应基类
- * @author aki
- * 2016年4月26日 上午11:54:43
+ *
  * @param <T>
  * @param <E>
+ * @author aki
+ * 2016年4月26日 上午11:54:43
  */
-public abstract class Response<T, E>{
-	protected T data;//返回数据
-	protected E error;//错误信息
-	protected String message;//提示信息
-	protected String code;//错误代码
-	
-	public T getData() {
-		return data;
-	}
-	public void setData(T data) {
-		this.data = data;
-	}
+@Data
+public abstract class Response<T, E> {
+	@ApiModelProperty("返回数据")
+	protected T data;
+	@ApiModelProperty("错误信息")
+	protected E error;
+	@ApiModelProperty("提示信息")
+	protected String message;
+	@ApiModelProperty("错误代码")
+	protected String code;
+
 	/**
 	 * 是否成功
+	 *
 	 * @return
 	 */
-	public boolean isSuccess(){
-		return error==null&&code==null&&message==null&&data!=null;
+	public boolean isSuccess() {
+		return error == null && code == null && message == null && data != null;
 	}
-	public E getError() {
-		return error;
-	}
-	public void setError(E error) {
-		this.error = error;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public String getCode() {
-		return code;
-	}
-	public void setCode(String code) {
-		this.code = code;
-	}
-	
 }
